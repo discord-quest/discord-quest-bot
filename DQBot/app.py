da995b9690f38cfd9c040e22dbdb58003eaaaa63
Oscar <oscar.shrimpton.personal@gmail.com>
2019-06-25 09:59:30 +0100

    Changed the way running works so teardown is now done properly

--- DQBot/app.py
+++ DQBot/app.py
@@ -1,19 +1,51 @@
-from . import dcord, db
+from .server import DataStore, RenderServer
+from os import getenv
+
 import asyncio
-from dotenv import load_dotenv
-from os import path
+import tortoise
+from tortoise import Tortoise
 
-# TODO: Proper settings setup
-# Includes environment variables from .env at root
-load_dotenv(path.join(path.dirname(__file__), '..', '.env'))
+class App:
+	async def run(self):
+		# database init
+		await self.db_start()
 
-# Start main event loop
-loop = asyncio.get_event_loop()
+		# rendering
+		self.store = DataStore()
+		self.server = RenderServer(self.store)
 
-loop.run_until_complete(db.setup())
+		await self.server.setup()
+		
+		# TODO: Discord.py setup
 
-loop.run_until_complete(dcord.run())
+		# For testing rendering quickly
+		# from .models import Player, ActiveWorld, PlayerEntity
+		# player = Player(discord_id="99999999")
+		# await player.save()
+		# player_entity = PlayerEntity(x=5, y=5)
+		# await player_entity.save()
+		# activeworld = ActiveWorld(world_name="test", player=player, player_entity=player_entity)
+		# await activeworld.save()
 
-loop.run_until_complete(db.teardown())
+		## or:
+		## activeworld = await ActiveWorld.first()
+		
+		# print(server.add_to_queue(activeworld))
 
-loop.close()
+		# keep everything alive by sleeping forever
+		while True:
+			await asyncio.sleep(3600)
+
+	async def db_start(self):
+	    await Tortoise.init(
+	        db_url=getenv('DB_URL'), # TODO
+	        modules={'models': ['DQBot.models']}
+	    )
+
+	    await Tortoise.generate_schemas()
+
+	async def teardown(self):
+		print("Tearing down app")
+
+		await Tortoise.close_connections()
+		await self.server.teardown()
--- run.py
+++ run.py
@@ -1,1 +1,23 @@
-import DQBot
+from DQBot import App
+from os import path
+
+import asyncio
+from dotenv import load_dotenv
+
+# TODO: Proper settings setup
+# Includes environment variables from .env at root
+load_dotenv(path.join(path.dirname(__file__), '.env'))
+
+# Start main event loop
+loop = asyncio.get_event_loop()
+
+app = App()
+try:
+	loop.run_until_complete(app.run())
+except Exception as e:
+	print(e)
+	pass
+finally:
+	loop.run_until_complete(app.teardown())
+
+loop.close()
