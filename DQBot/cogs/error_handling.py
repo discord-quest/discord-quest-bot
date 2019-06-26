f424764f7c591dc1da46180d025ab90324bdc1c0
Sylvia Mills <CHATALOT5000000000@gmail.com>
2019-06-27 00:14:39 +0100

    Added error reporting

--- DQBot/cogs/error_handling.py
+++ DQBot/cogs/error_handling.py
@@ -1,5 +1,7 @@
-from DQBot.utils import get_dev_IDs
+from DQBot.utils import get_dev_IDs, add_reactions
 from traceback import format_exception
+from asyncio import TimeoutError
+from os import getenv
 
 from discord.ext import commands
 
@@ -25,10 +27,51 @@ class ErrorHandling(commands.Cog):
                 await ctx.author.send(
                     f"```py\n{''.join(format_exception(type(exc), exc, exc.__traceback__))}\n```"
                 )
+                return
 
-            # TODO: error reporting for non-devs
+            await self.handle_error_reporting(ctx, exc)
+            return
 
         await ctx.send(exc)
+
+    async def handle_error_reporting(self, ctx, exc):
+        reactions = ["\U0001f1fe", "\U0001f1f3"]
+        report_channel = self.client.get_channel(int(getenv("ERROR_CHANNEL_ID")))
+
+        confirm_report_msg = await ctx.send("An unexpected error occurred, report?")
+        await add_reactions(confirm_report_msg, *reactions)
+
+        # TODO: Abstract away the below for use here and in the main bot features
+        try:
+            response, user = await self.client.wait_for(
+                "reaction_add",
+                check=lambda r, u: u.id == ctx.author.id
+                and r.emoji in reactions
+                and r.message.id == confirm_report_msg.id
+                and r.message.channel.id == confirm_report_msg.channel.id,
+                timeout=30,
+            )
+
+        except TimeoutError:
+            await confirm_report_msg.edit(
+                content=f"~~{confirm_report_msg.content}~~ Time's up!"
+            )
+            return
+
+        if response.emoji == reactions[0]:
+            await report_channel.send(
+                f"Exception raised:\nUser: {ctx.author} ({ctx.author.id})"
+                f"\nCommand: {ctx.command}\nCog: {ctx.command.cog_name}"
+                f"\nTraceback: ```py\n{''.join(format_exception(type(exc), exc, exc.__traceback__))}\n```"
+            )
+            await confirm_report_msg.edit(
+                content=f"~~{confirm_report_msg.content}~~ Error reported!"
+            )
+
+        else:
+            await confirm_report_msg.edit(
+                content=f"~~{confirm_report_msg.content}~~ Error ignored."
+            )
 
 
 def setup(client):
--- DQBot/utils.py
+++ DQBot/utils.py
@@ -1,4 +1,10 @@
 from os import getenv
 
+
 def get_dev_IDs():
     return [int(f) for f in getenv("DEV_IDS").split(";")]
+
+
+async def add_reactions(message, *reacs):
+    for reac in reacs:
+        await message.add_reaction(reac)
