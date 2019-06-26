f5a451cb38573456eb0c1a61e4d77fb772be2a51
Sylvia Mills <CHATALOT5000000000@gmail.com>
2019-06-26 19:19:10 +0100

    Improved bot setup
     cogs/extensions/commands/everything to come

--- DQBot/app.py
+++ DQBot/app.py
@@ -1,35 +1,35 @@
 from .server import DataStore, RenderServer
 from .inventory import ItemStore
-from .dcord import Quest
-from os import getenv
+from os import getenv, listdir, path
 import logging
 import asyncio
 import tortoise
 from tortoise import Tortoise
 import random
 
-from discord.ext.commands import Bot
+from discord.ext import commands
 
 logger = logging.getLogger("app")
 
 # For testing stuff
 # async def test_chest(store, item_store):
-    # from .models import Player, ActiveWorld, PlayerEntity, ChestEntity
-    # from .action import Action, Direction
+#     from .models import Player, ActiveWorld, PlayerEntity, ChestEntity
+#     from .action import Action, Direction
+#
+#     player = Player(discord_id=str(random.random() * 100))
+#     await player.save()
+#     player_entity = PlayerEntity(x=4, y=4)
+#     await player_entity.save()
+#     activeworld = ActiveWorld(world_name="test", player=player, player_entity=player_entity)
+#     await activeworld.save()
+#
+#     chest = ChestEntity(x=5, y=4, level=1, active_world=activeworld)
+#     await chest.save()
+#
+#     print(await player_entity.inventory.all())
+#     await activeworld.take_action(Action.open_chest(Direction.RIGHT), store.worlds[activeworld.world_name], item_store)
+#     print(await player_entity.inventory.all())
 
-    # player = Player(discord_id=str(random.random() * 100))
-    # await player.save()
-    # player_entity = PlayerEntity(x=4, y=4)
-    # await player_entity.save()
-    # activeworld = ActiveWorld(world_name="test", player=player, player_entity=player_entity)
-    # await activeworld.save()
-
-    # chest = ChestEntity(x=5, y=4, level=1, active_world=activeworld)
-    # await chest.save()
-
-    # print(await player_entity.inventory.all())
-    # await activeworld.take_action(Action.open_chest(Direction.RIGHT), store.worlds[activeworld.world_name], item_store)
-    # print(await player_entity.inventory.all())
 
 class App:
     async def run(self):
@@ -47,10 +47,14 @@ class App:
 
         # await test_chest(self.store, self.item_store)
 
-        self.bot = Bot(command_prefix=";")
-        self.cog = Quest(self.bot, self)
-
-        self.bot.add_cog(self.cog)
+        self.bot = commands.Bot(command_prefix=";")
+        for filename in listdir(
+            path.join(path.dirname(path.realpath(__file__)), "cogs")
+        ):
+            try:
+                self.bot.load_extension(filename)
+            except commands.ExtensionError as e:
+                logger.error(e)
 
         await self.bot.start(getenv("BOT_TOKEN"))
 
