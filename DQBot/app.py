2b63a701e9ab580fa8342f78bf5c7ccd1348b1d8
Sylvia Mills <CHATALOT5000000000@gmail.com>
2019-06-26 23:10:07 +0100

    Removed the test comments
    You can use `jsk py` for that now

--- DQBot/app.py
+++ DQBot/app.py
@@ -13,26 +13,6 @@ from discord.ext import commands
 
 logger = logging.getLogger("app")
 
-# For testing stuff
-# async def test_chest(store, item_store):
-#     from .models import Player, ActiveWorld, PlayerEntity, ChestEntity
-#     from .action import Action, Direction
-#
-#     player = Player(discord_id=str(random.random() * 100))
-#     await player.save()
-#     player_entity = PlayerEntity(x=4, y=4)
-#     await player_entity.save()
-#     activeworld = ActiveWorld(world_name="test", player=player, player_entity=player_entity)
-#     await activeworld.save()
-#
-#     chest = ChestEntity(x=5, y=4, level=1, active_world=activeworld)
-#     await chest.save()
-#
-#     print(await player_entity.inventory.all())
-#     await activeworld.take_action(Action.open_chest(Direction.RIGHT), store.worlds[activeworld.world_name], item_store)
-#     print(await player_entity.inventory.all())
-
-
 class App:
     async def run(self):
         logger.info("Initialising...")
@@ -46,8 +26,6 @@ class App:
         self.server = RenderServer(self.store)
 
         await self.server.setup()
-
-        # await test_chest(self.store, self.item_store)
 
         class DQBot(commands.Bot):
             # Overwrites default owner check to allow for multiple owners as defined in .env
